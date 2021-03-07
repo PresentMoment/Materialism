571eef121332891453adf70abb547d3eaa6fa995
Robert Girardin <robertlgirardin@gmail.com>
2021-03-06 21:10:32 -0800

    replace mapquest call

--- Components/Content/ArtistCard.tsx
+++ Components/Content/ArtistCard.tsx
@@ -7,30 +7,32 @@ import client from '../../client'
 const builder = imageUrlBuilder(client);
 export default function ArtistCard(props) {
   const  artWorks = props.props;
-  console.log(artWorks)
-  const isBreakPoint = useMediaQuery(425);
+  const isBreakPoint = useMediaQuery(768);
   return (
     <>
     <Wrapper isBreakPoint={isBreakPoint}>
       <div style={{display: 'flex', flexDirection: "column", width: '100%'}}>
       {artWorks.map((artwork)=> {
         return(
-          <div style={{display: 'flex', flexDirection: 'row', marginBottom: '1em'}}>
-      <ArtistInfo isBreakPoint={isBreakPoint}>
-        <span>Artist: {artwork.artist.name}</span>
-        <span>Title: {artwork.title}</span>
-         <span>Year: {artwork.year}</span>
-         </ArtistInfo>
-         <img
-          src={builder.image(artwork.image).auto("format").width(isBreakPoint ? 200 : 100).height(isBreakPoint ? 200 : 100).url()}
-          alt={""}
-          />
+          <div key={artwork._id}>
+        <div style={{display: 'flex', flexDirection: 'row'}} key={artwork._id}>
+            <ArtistInfo isBreakPoint={isBreakPoint} key={artwork._id}>
+            <span>Artist: {artwork.artist.name}</span>
+            <span>Title: {artwork.title}</span>
+            <span>Year: {artwork.year}</span>
+            </ArtistInfo>
+            <img
+              src={builder.image(artwork.image).auto("format").width(isBreakPoint ? 100 : 200).height(isBreakPoint ? 100 : 200).url()}
+              alt={""}
+            />
+            </div>
+            <LineBreak width='100%' />
+            <div style={{height: '20px'}} />
           </div>
         )
       })}
         </div>
     </Wrapper>
-      <LineBreak />
       </>
   )
 }
@@ -46,13 +48,13 @@ const ArtistInfo = styled("div")<{isBreakPoint: boolean}>`
 display: flex;
 flex-direction: column;
 font-family: 'Raleway';
-font-size: ${(p) => p.isBreakPoint ? '0.7em' : '1.5em'};
+font-size: ${(p) => p.isBreakPoint ? '0.9em' : '1.5em'};
 line-height: 2em;
 flex: 1;
 `
-const LineBreak =  styled('div')`
+const LineBreak =  styled('div')<{width: string}>`
 border-bottom: 1px solid black;
 padding-bottom: 20px;
 margin: 0 auto;
-width: 95%;
+width: ${(p) => p.width};
 `
