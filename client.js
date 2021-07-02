8f518f220da5e26b40609407824374bd257a7a70
Robert Girardin <robertlgirardin@gmail.com>
2021-07-02 12:40:45 -0400

    out folder

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const sanityClient = require("@sanity/client")
 const client = sanityClient({
   projectId: "mquooija",
   dataset: "production",
-  apiVersion: "2021-07-02",
+  apiVersion: "2021-07-01",
   token: "", // or leave blank to be anonymous user
   useCdn: true, // `false` if you want to ensure fresh data
 })
