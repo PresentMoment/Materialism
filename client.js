45051ee658b684bdbdca7b4b75b809b6f9a5a6e1
Robert Girardin <robertlgirardin@gmail.com>
2021-07-02 12:22:59 -0400

    removesanity troken

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const client = sanityClient({
   projectId: "mquooija",
   dataset: "production",
   apiVersion: "2021-07-02",
-  token: process.env.SANITY_WRITE_TOKEN, // or leave blank to be anonymous user
+  token: "", // or leave blank to be anonymous user
   useCdn: true, // `false` if you want to ensure fresh data
 })
 
