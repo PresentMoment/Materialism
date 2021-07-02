56394d0aeed81bd7c435d79e03d1d0f7e0fe8deb
Robert Girardin <robertlgirardin@gmail.com>
2021-07-02 12:58:26 -0400

    ignore add page

--- pages/add/index.tsx
+++ pages/add/index.tsx
@@ -1,51 +1,51 @@
-// import React, { useState, useEffect } from 'react'
-// import styled from "styled-components"
-// import groq from "groq";
-// import client from "../../client";
-// import { useRouter } from "next/router";
-// import Layout from '../../Components/Layout';
-// import Autocomplete from './Autocomplete';
+import React, { useState, useEffect } from 'react'
+import styled from "styled-components"
+import groq from "groq";
+import client from "../../client";
+import { useRouter } from "next/router";
+import Layout from '../../Components/Layout';
+import Autocomplete from './Autocomplete';
 
-// const artistQuery = groq`
-// *[_type == 'artist']`;
+const artistQuery = groq`
+*[_type == 'artist']`;
 
 
-// function addAnArtWork({data = {}}) {
+function addAnArtWork({data = {}}) {
 
-//   const [ artist, setArtist ] = useState({_type: 'artist', name: ""})
-//   const handleChange = (event) => {
-//     setArtist({...artist, name: event.target.value.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())})
-//   }
-//   const handleSubmit = () => {
-//     event.preventDefault();
-//     console.log('submitting' + artist)
-//     client.create(artist).then((res) => {
-//       console.log(res)
-//     })
-//     .catch((error) => {
-//       console.log(error.message)
-//     })
-//   }
-//   return (
-//     <Layout>
-//       <Autocomplete data={data} />
-//       <form onSubmit={handleSubmit}><input 
-//         onChange={handleChange}
-//         type="text"
-//         id="artist"
-//         autoFocus={false}
-//         placeholder={'Artist Name'}
-//         ></input>
-//         <button type="submit">SUBMIT</button>
-//         </form>
-//     </Layout>
-//   )
-// }
+  const [ artist, setArtist ] = useState({_type: 'artist', name: ""})
+  const handleChange = (event) => {
+    setArtist({...artist, name: event.target.value.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())})
+  }
+  const handleSubmit = () => {
+    event.preventDefault();
+    console.log('submitting' + artist)
+    client.create(artist).then((res) => {
+      console.log(res)
+    })
+    .catch((error) => {
+      console.log(error.message)
+    })
+  }
+  return (
+    <Layout>
+      <Autocomplete data={data} />
+      <form onSubmit={handleSubmit}><input 
+        onChange={handleChange}
+        type="text"
+        id="artist"
+        autoFocus={false}
+        placeholder={'Artist Name'}
+        ></input>
+        <button type="submit">SUBMIT</button>
+        </form>
+    </Layout>
+  )
+}
 
-// export async function getServerSideProps() {
-//   const res = await client.fetch(artistQuery);
-//   const json = await res;
-//   return { props: { data: json } };
-// }
+export async function getServerSideProps() {
+  const res = await client.fetch(artistQuery);
+  const json = await res;
+  return { props: { data: json } };
+}
 
-// export default addAnArtWork;
+export default addAnArtWork;
