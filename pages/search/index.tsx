df195489cd66023df862eacfc4c1b051229ec68e
Robert Girardin <robertlgirardin@gmail.com>
2021-07-09 15:50:07 -0400

    mobile styles for search results

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import dynamic from 'next/dynamic'
 import { NextSeo } from "next-seo";
 import styled from "styled-components"
@@ -29,6 +29,13 @@ function SearchResults({ config, page }) {
   const router = useRouter();
   const isBreakPoint = useMediaQuery(425);
   const searchQuery = router.query.q as string;
+  const artistCard = useRef(null);
+  const [gradientWidth, setGradientWidth] = useState(null)
+
+  useEffect(() => {
+    setGradientWidth(artistCard.current.offsetWidth);
+  }, [artistCard, gradientWidth])
+
   return (
       <Layout>
         <NextSeo
@@ -38,8 +45,15 @@ function SearchResults({ config, page }) {
         description="Materialism - art within reach"
       />
          <ContentContainer isBreakPoint={isBreakPoint}>
+         
         {page.length > 0 ?
+        <ContentList isBreakPoint={isBreakPoint}>
+        <div ref={el => { artistCard.current = el}} style={{width: '100%'}} >
+
         <ArtistCard props={page} flex={2} />
+        </div>
+        <GradientWrapper gradientWidth={gradientWidth}><Gradient /></GradientWrapper>
+      </ContentList>
         : <div style={{width: '100%', textAlign: 'center'}}><span>No Results</span></div>}
       <div style={{display: 'flex', flex: 1}}>
 
@@ -56,25 +70,38 @@ height: 100%;
 margin: 10px ${(p) => p.isBreakPoint ? '4px' : '30px'};
 border: 1px solid black;
 padding: ${(p) => p.isBreakPoint ? '4px' : '10px'};
-height: ${(p) => p.isBreakPoint ? '85vh' :'80vh'};
+height: ${(p) => p.isBreakPoint ? '90vh' :'80vh'};
 `
 
-// export async function getStaticProps(paths) {
-//   const pid = paths.params.artist;
-//   const res = await client.fetch(pageQuery, { pid });
-//   const json = await res;
-//   return { props: { data: json } };
-// }
+const ContentList = styled.div<{isBreakPoint: boolean}>`
+width: ${(p) => p.isBreakPoint ? '100%' : '40vw'};
+height: ${(p) => p.isBreakPoint ? '48vh': '100%'};
+display: flex;
+flex: 1;
+overflow-y: scroll;
+position: relative;
+`
 
-// export async function getStaticPaths() {
-//   const request = await client.fetch(
-//     `*[_type == "artist" && defined(name)][].name`
-//   );
-//   return {
-//     paths: request.map((slug) => ({ params: { artist: `${slug}` } })),
-//     fallback: false,
-//   };
-// }
+const GradientWrapper = styled.div<{gradientWidth: number}>`
+height: 100px;
+width: ${(p) => p.gradientWidth}px;
+position: absolute;
+margin-left: auto;
+margin-right: auto;
+bottom: 0;
+//left: 0;
+//right: 0;
+//top: 41.5%;
+z-index: 9;
+`
+const Gradient = styled('div')`
+height: 100px;
+width: inherit;
+position: fixed;
+z-index: 9;
+background-color: rgba(255,255,255,0);
+background-image: linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255,1));
+`
 
 SearchResults.getInitialProps = async (context) => {
   const slug = context.pathname.substring(1);
