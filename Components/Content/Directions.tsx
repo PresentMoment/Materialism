5b37af88d950ec99373bd030c26b2e5cf3fb9ab1
Robert Girardin <robertlgirardin@gmail.com>
2021-07-16 18:59:44 -0400

    directions fetching working

--- Components/Content/Directions.tsx
+++ Components/Content/Directions.tsx
@@ -1,30 +1,36 @@
-import { Console } from 'console';
 import React, {useState} from 'react'
 import styled from "styled-components"
 
 export default function Directions(props) {
   const lat = props.data.location.lat;
   const lng = props.data.location.lng;
-  const [userlocation, setUserLocation] = useState([undefined, undefined])
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchUserLocation = () => {
+    setIsFetching(true)
     const options = {timeout: 7000}
-    navigator.geolocation.getCurrentPosition(showPosition, locationError, options);
+    return new Promise(() => {
+      navigator.geolocation.getCurrentPosition(showPosition, locationError, options);
+    });
   }
   function locationError(err){
     console.log([`Location service not available in this browser`, '(try viewing Materialism in Chrome or Safari)'])
   }
   
     function showPosition(position) {
+      setIsFetching(false)
       var win = window.open(`https://www.google.com/maps/dir/?api=1&destination=${lat}%2C${lng}&origin=${position.coords.latitude}%2C${position.coords.longitude}`, '_blank');
       win.focus();
     }
 
+  const handleClick = () => {
+    fetchUserLocation()
+  }
   return (
-    <Container onClick={fetchUserLocation}>
+    <Container onClick={handleClick}>
       {/* <a target="_blank"  */}
     {/* href={`https://www.google.com/maps/dir/?api=1&destination=${lat}%2C${lng}${userlocation[0] !== undefined ? `&origin=${userlocation[0]}%2C${userlocation[1]}` : ''}`} rel="noopener noreferrer"> */}
-    <span>Directions</span>
+    <span>{!isFetching ? 'Directions' : 'Fetching location...map will open in new window'}</span>
     {/* </a> */}
     </Container>
   )
