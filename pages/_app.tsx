58e54db192f1380a820d89cd2297bf38f2a74c58
Robert Girardin <robertlgirardin@gmail.com>
2021-08-04 16:53:05 -0400

    FBAV regex insta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ function Materialism({ Component, pageProps }: AppProps) {
       console.log(ua);
       const regexSafariMobile = /(?=^.*Safari)(?=^.*Mobile)(?!^.*CriOS)(?!^.*Firefox)(?!^.*Chrome)(?!^.*Edg)(?!^.*Linux).*/;
       const regexSafariDesktop = /(?=^.*Safari)(?=^.*Macintosh)(?!^.*Chrome)(?!^.*Linux).*/;
-      const regexInsta = /(?=^.*FB4A).*/;
+      const regexInsta = /(?=^.*FBAV).*/;
       let safariMobiletest = regexSafariMobile.test(ua);
       let safariDesktopTest = regexSafariDesktop.test(ua);
       let instaTest = regexInsta.test(ua);
