d7efc226218e9167dc5cea30ef3c278e95091bf9
Robert Girardin <robertlgirardin@gmail.com>
2021-08-13 10:22:22 -0400

    update client

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const client = sanityClient({
   projectId: "mquooija",
   dataset: "production",
   apiVersion: "2021-07-01",
-  token: process.env.SANITY_WRITE_TOKEN, // or leave blank to be anonymous user
+  token: process.env.SANITY_WRITE_TOKEN,
   useCdn: false, // `false` if you want to ensure fresh data
 })
 
